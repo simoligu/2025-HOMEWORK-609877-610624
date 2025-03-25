5eb8b6d0482f2b1e5bc20dfd13818ef948639c3f
Diegozzone <diego.polisini@gmail.com>
2025-03-25 16:18:42 +0100

    Verifica

---
+++ src/Labirinto.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Labirinto {
+	//ha la responsabilità di creare il labirinto, di memorizzare la stanza iniziale (entrata) e quella finale (uscita)
+	public String entrate;
+	public String finale;
+	public List<String> stanze;
+		
+	public Labirinto() {
+		this.stanze=new ArrayList<>();
+	}
+}
+	public void creaLabirinto() {
+		/* crea stanze del labirinto */
+		Stanza atrio = new Stanza("Atrio");
+		Stanza aulaN11 = new Stanza("Aula N11");
+		Stanza aulaN10 = new Stanza("Aula N10");
+		Stanza laboratorio = new Stanza("Laboratorio Campus");
+		Stanza biblioteca = new Stanza("Biblioteca");
+		
+		/* collega le stanze */
+		atrio.impostaStanzaAdiacente("nord", biblioteca);
+		atrio.impostaStanzaAdiacente("est", aulaN11);
+		atrio.impostaStanzaAdiacente("sud", aulaN10);
+		atrio.impostaStanzaAdiacente("ovest", laboratorio);
+		aulaN11.impostaStanzaAdiacente("est", laboratorio);
+		aulaN11.impostaStanzaAdiacente("ovest", atrio);
+		aulaN10.impostaStanzaAdiacente("nord", atrio);
+		aulaN10.impostaStanzaAdiacente("est", aulaN11);
+		aulaN10.impostaStanzaAdiacente("ovest", laboratorio);
+		laboratorio.impostaStanzaAdiacente("est", atrio);
+		laboratorio.impostaStanzaAdiacente("ovest", aulaN11);
+		biblioteca.impostaStanzaAdiacente("sud", atrio);
+		
+			
+	}
+
