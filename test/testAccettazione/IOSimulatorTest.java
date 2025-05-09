b690721104e202278be621b602f7cd0d0143d823
simoligu <simone.ligurso@gmail.com>
2025-05-09 14:56:05 +0200

    IOSimulator

---
+++ test/testAccettazione/IOSimulatorTest.java
@@ -0,0 +1,23 @@
+package testAccettazione;
+
+import org.junit.Test;
+
+import it.uniroma3.diadia.DiaDia;
+import it.uniroma3.diadia.IOSimulator;
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+public class IOSimulatorTest {
+	@Test
+	public IOSimulator creaSimulazionePartitaEGioca(String... comandiDaLeggere) {
+		IOSimulator io = new IOSimulator(comandiDaLeggere);
+		new DiaDia(io).gioca();
+		return io;
+	}
+	@Test
+	public Attrezzo creaAttrezzoEAggiugniAStanza(Stanza stanzaDaRiempire, String nomeAttrezzo, int peso) {
+		Attrezzo attrezzo = new Attrezzo(nomeAttrezzo, peso);
+		stanzaDaRiempire.addAttrezzo(attrezzo);
+		return attrezzo;
+	}
+}
