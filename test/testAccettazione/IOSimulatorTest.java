152e0702bf9d72124f63878b1780a838d33be15c
Diegozzone <diego.polisini@gmail.com>
2025-05-09 15:05:07 +0200

    testIOSimulator

--- test/testAccettazione/IOSimulatorTest.java
+++ test/testAccettazione/IOSimulatorTest.java
@@ -1,6 +1,7 @@
 package testAccettazione;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
 
 import it.uniroma3.diadia.DiaDia;
 import it.uniroma3.diadia.IOSimulator;
@@ -9,15 +10,23 @@ import it.uniroma3.diadia.attrezzi.Attrezzo;
 
 public class IOSimulatorTest {
 	@Test
-	public IOSimulator creaSimulazionePartitaEGioca(String... comandiDaLeggere) {
-		IOSimulator io = new IOSimulator(comandiDaLeggere);
-		new DiaDia(io).gioca();
-		return io;
-	}
-	@Test
-	public Attrezzo creaAttrezzoEAggiugniAStanza(Stanza stanzaDaRiempire, String nomeAttrezzo, int peso) {
-		Attrezzo attrezzo = new Attrezzo(nomeAttrezzo, peso);
-		stanzaDaRiempire.addAttrezzo(attrezzo);
-		return attrezzo;
+	public void testSimulazionePartita() {
+		String[] comandi = {"vai nord", "prendi chiave", "fine"};
+		IOSimulator io = new IOSimulator(comandi);
+
+		// Simulazione di gioco: solo per esempio
+		io.mostraMessaggio("Sei nell'atrio");
+		assertEquals("vai nord", io.leggiRiga());
+
+		io.mostraMessaggio("Sei in aula N11");
+		assertEquals("prendi chiave", io.leggiRiga());
+
+		io.mostraMessaggio("Hai preso la chiave");
+		assertEquals("fine", io.leggiRiga());
+
+		assertEquals("Sei nell'atrio", io.nextMessaggio());
+		assertEquals("Sei in aula N11", io.nextMessaggio());
+		assertEquals("Hai preso la chiave", io.nextMessaggio());
+		assertFalse(io.hasNextMessaggio());
 	}
 }
